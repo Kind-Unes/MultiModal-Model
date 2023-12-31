cb832b7ddd0d1377317fd02ea04c732bf2737b66
Kind.Unes <my_hellalet@esi.dz>
2023-12-31 13:33:11 +0100

    added text generation function (role + prompt)

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import requests
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-import io
 from PIL import Image
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -18,6 +18,61 @@ load_dotenv()
 txt2img_api_token = os.getenv("API_TOKEN_SSD_1B_ANIME")
 authorization = os.getenv("HEADER_AUTH")
 headers = {"Authorization": authorization}
+GOOGLE_AI_STUDIO = os.getenv("API_TOKEN_GOOGLE_AI_STUDIO")
+
+
+# Text Generation 
+genai.configure(api_key=GOOGLE_AI_STUDIO)
+
+# Set up the model
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "block_none"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "block_none"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "block_none"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "block_none"},
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-pro",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
+
+
+
+def generate_content(role, prompt):
+    prompt_parts = [
+        role, prompt
+    ]
+
+    response = model.generate_content(prompt_parts)
+
+    return response.text
+
+
+@app.route('/generate', methods=['POST'])
+def generate_text():
+        
+    try:
+        data = request.json
+        role = data["role"]
+        prompt = data["prompt"]
+
+
+
+        result = generate_content(role,prompt)
+   
+        return jsonify({"status": "success", "result": result})
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": f"Error generating text: {str(e)}"}),500
 
 
 
@@ -46,7 +101,7 @@ def generate_image():
 
     except Exception as e:
         # Return an error response if an exception occurs
-        return jsonify({"status": "error", "message": f"Error generating image: {str(e)}"})
+        return jsonify({"status": "error", "message": f"Error generating image: {str(e)}"}),500
 
 
 # IMAGE TO TEXT 
